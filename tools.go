5ed881d9be6f244ab598193b6d9e577eec4b584d
Josiah Niedrauer <jniedrauer@gmail.com>
2019-02-26 15:33:08 -0800

    Update path to golint

--- tools.go
+++ tools.go
@@ -4,8 +4,8 @@ package tools
 
 import (
 	// Build and CI/CD tools
-	_ "github.com/golang/lint/golint"
 	_ "github.com/josephspurrier/goversioninfo"
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mgechev/revive"
+	_ "golang.org/x/lint/golint"
 )
