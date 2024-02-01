f9aa125bed5fe1af61e292818a3ae0cae5f39b0d
BlockChyp SDK Builder <devops@blockchyp.com>
2024-02-01 18:12:54 +0000

    Merge pull request #260 from blockchyp/feature/javaBug
    Java test bug and php null exception fix

---
+++ pkg/itests/merchant_credential_generation_test.go
@@ -0,0 +1,42 @@
+//go:build integration
+// +build integration
+
+// Copyright 2019-2024 BlockChyp, Inc. All rights reserved. Use of this code
+// is governed by a license that can be found in the LICENSE file.
+//
+// This file was generated automatically by the BlockChyp SDK Generator.
+// Changes to this file will be lost every time the code is regenerated.
+
+package itests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	blockchyp "github.com/blockchyp/blockchyp-go/v2"
+)
+
+func TestMerchantCredentialGeneration(t *testing.T) {
+	assert := assert.New(t)
+
+	config := loadTestConfiguration(t)
+	client := config.newTestClient(t, "")
+
+	// setup request object
+	request := blockchyp.MerchantCredentialGenerationRequest{
+		Test:       true,
+		MerchantID: "<MERCHANT ID>",
+	}
+
+	logObj(t, "Request:", request)
+
+	response, err := client.MerchantCredentialGeneration(request)
+
+	assert.NoError(err)
+
+	logObj(t, "Response:", response)
+
+	// response assertions
+	assert.True(response.Success)
+}
