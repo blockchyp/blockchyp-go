a8dafa349e7658755d43a8f367250e5a58d0c782
BlockChyp SDK Builder <devops@blockchyp.com>
2026-08-04 18:15:08 +0000

    feat(VIP-1184): add status to CaptureResponse and VoidResponse. document Enroll endpoint ignores Token field (#377)

--- models.go
+++ models.go
@@ -1159,7 +1159,7 @@ type AuthorizationRequest struct {
 	TestCase string `json:"testCase,omitempty"`
 
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string `json:"token,omitempty"`
 
 	// Track1 contains track 1 magnetic stripe data.
@@ -1532,7 +1532,7 @@ type CardMetadataRequest struct {
 	TestCase string `json:"testCase,omitempty"`
 
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string `json:"token,omitempty"`
 
 	// Track1 contains track 1 magnetic stripe data.
@@ -1729,7 +1729,7 @@ type BalanceRequest struct {
 	TestCase string `json:"testCase,omitempty"`
 
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string `json:"token,omitempty"`
 
 	// Track1 contains track 1 magnetic stripe data.
@@ -1952,7 +1952,7 @@ type RefundRequest struct {
 	TestCase string `json:"testCase,omitempty"`
 
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string `json:"token,omitempty"`
 
 	// Track1 contains track 1 magnetic stripe data.
@@ -2327,6 +2327,9 @@ type CaptureResponse struct {
 	// Customers contains customer data, if any.
 	Customers []Customer `json:"customers"`
 
+	// Status indicates the current status of a transaction.
+	Status string `json:"status,omitempty"`
+
 	// EnhancedDataPassed indicates whether enhanced data was passed for the
 	// transaction.
 	EnhancedDataPassed *bool `json:"enhancedDataPassed,omitempty"`
@@ -2501,6 +2504,9 @@ type VoidResponse struct {
 
 	// SigFile contains the hex encoded signature data.
 	SigFile string `json:"sigFile,omitempty"`
+
+	// Status indicates the current status of a transaction.
+	Status string `json:"status,omitempty"`
 }
 
 // EnrollRequest contains the information needed to enroll a new payment
@@ -2550,7 +2556,7 @@ type EnrollRequest struct {
 	TestCase string `json:"testCase,omitempty"`
 
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string `json:"token,omitempty"`
 
 	// Track1 contains track 1 magnetic stripe data.
@@ -9070,7 +9076,7 @@ func (r PaymentAmounts) From(raw interface{}) (result PaymentAmounts, ok bool) {
 // PaymentMethod contains request details about a payment method.
 type PaymentMethod struct {
 	// Token is the payment token to be used for this transaction. This should be
-	// used for recurring transactions.
+	// used for recurring transactions. The /enroll endpoint ignores this field.
 	Token string
 
 	// Track1 contains track 1 magnetic stripe data.
